c05008fc4c0abdd8615e636d7472061d36db224d
gauravk1302 <gauravsaini77069@gmail.com>
2025-01-07 06:55:28 +0530

    added my python files

---
+++ tuple_count_example.py
@@ -0,0 +1,8 @@
+#to count hte number of getting grade A in he given tuple
+tup=("C","D","A","A","B","B","A")
+print(tup.count("A"))
+
+# to sort the same tuple by taking it as list
+grade=["C","D","A","A","B","B","A"]
+grade.sort()
+print(grade)
