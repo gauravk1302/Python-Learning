e7ac913765133822d006cdd3465714cd7b80200d
gauravk1302 <gauravsaini77069@gmail.com>
2025-01-09 07:12:51 +0530

    added my python files

---
+++ check_palindrome.py
@@ -0,0 +1,8 @@
+#to check if a list has palindrome elements or not
+list1=[1,2,1]
+copy_list1=list1.copy()
+copy_list1.reverse()
+if(copy_list1==list1):
+    print("palindrome")
+else:
+    print("not palindrome")
